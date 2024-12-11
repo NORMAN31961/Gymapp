b6cb7589e895f80804c189ef1a8272590fab709c
MarquezJesus24 <thegamerdie83@gmail.com>
2024-12-10 19:17:43 -0500

    Arreglo de vista de la activity_home.xml

--- app/src/main/java/com/example/gym/HomeActivity.kt
+++ app/src/main/java/com/example/gym/HomeActivity.kt
@@ -2,9 +2,8 @@ package com.example.gym
 
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.ImageButton
-import android.widget.Toast
-import android.widget.Toast.makeText
+import android.view.ViewGroup
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -12,36 +11,46 @@ import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
+import com.example.gym.databinding.ActivityHomeBinding
+
 
 class HomeActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityHomeBinding
     lateinit var drawerLayout: DrawerLayout
     lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout_Home_Customer)) { v, insets ->
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        drawerLayout = findViewById(R.id.drawer_layout_Home_Customer)
-        actionBarDrawerToggle =
-            ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
-        val menuIcon: ImageButton = findViewById(R.id.menu_icon)
+        binding.root.setOnApplyWindowInsetsListener { view, insets ->
+            val params = view.layoutParams as ViewGroup.MarginLayoutParams
+            params.topMargin = insets.systemWindowInsetTop
+            view.layoutParams = params
+            insets
+        }
+
+        drawerLayout = binding.drawerLayoutHomeCustomer
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
 
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
 
-        menuIcon.setOnClickListener {
-            drawerLayout.openDrawer(GravityCompat.START) // Abre el menú.
+        binding.menuIcon.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
         }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
