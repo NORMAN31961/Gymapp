7dc68b5381bdb14f58db988987bfdbcb852573dd
MarquezJesus24 <thegamerdie83@gmail.com>
2024-12-10 21:50:32 -0500

    Cambio home_activity a main_acitivy y lo mismo para home_coach_activity a CoachActivity

---
+++ app/src/main/java/com/example/gym/HomeCustomerFragment.kt
@@ -0,0 +1,84 @@
+package com.example.gym
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.gym.databinding.FragmentHomeCustomerBinding
+
+class HomeCustomerFragment : Fragment() {
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
+    private var _binding: FragmentHomeCustomerBinding? = null
+    private val binding get() = _binding!!
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentHomeCustomerBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initDrawer()
+        buttons_Navigation()
+    }
+
+    fun initDrawer() {
+        drawerLayout = binding.drawerLayoutHomeCustomer
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(
+                context as MainActivity,
+                drawerLayout,
+                R.string.open,
+                R.string.close
+            )
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        binding.menuIcon.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    fun buttons_Navigation() {
+        val navigationView = binding.navigationViewHomeCustomer
+
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_profile -> launchProfileFragment()
+
+                R.id.menu_settings -> launchSettingsFragment()
+
+                R.id.menu_home -> Toast.makeText(context, "Home", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    private fun launchProfileFragment(){
+        (activity as MainActivity).replaceFragment(ProfileFragment())
+    }
+
+    private fun launchSettingsFragment(){
+        (activity as MainActivity).replaceFragment(SettingsFragment())
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/gym/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.gym
+
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.example.gym.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        if (savedInstanceState == null) {
+            replaceFragment(HomeCustomerFragment())
+        }
+
+    }
+
+    fun replaceFragment(frag: Fragment) {
+        val fgTransaction = supportFragmentManager.beginTransaction()
+        fgTransaction.replace(R.id.main, frag)
+        fgTransaction.commit()
+    }
+
+
+}
