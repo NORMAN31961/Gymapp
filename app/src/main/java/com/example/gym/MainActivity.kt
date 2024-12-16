44bbaaec0f77bcc2b15312caa428b13443dac9ff
MarquezJesus24 <thegamerdie83@gmail.com>
2024-12-16 16:39:18 -0500

    si

--- app/src/main/java/com/example/gym/MainActivity.kt
+++ app/src/main/java/com/example/gym/MainActivity.kt
@@ -1,25 +1,12 @@
 package com.example.gym
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.gym.adapter.UsuarioAdapter
 import com.example.gym.databinding.ActivityMainBinding
-import com.example.gym.models.ViewModelUsuario
-import conexiondb
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
@@ -44,7 +31,6 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
 
     fun replaceFragment(frag: Fragment) {
         val fgTransaction = supportFragmentManager.beginTransaction()
